d33d444e4fff7b7fcca2cfc0ec586aacb67b5102
곽진규 <146896149+btnl426@users.noreply.github.com>
2024-08-29 05:50:38 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.10 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -0,0 +1,18 @@
+def solution(n, words):
+    startIndex = 0
+    count = 0
+    
+    for i in range(len(words)):
+        secIndex = 0
+        for num, word in enumerate(words[startIndex:startIndex+n]):
+            if count >= 1:
+                if words[count-1][-1] != words[count][0]:
+                    return num+1, i+1
+            if word in words[0:startIndex + secIndex]:
+                print("dfsdf")
+                return num+1, i+1
+            secIndex += 1
+            count += 1 
+        startIndex += n
+        
+    return 0, 0
