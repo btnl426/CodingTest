3077a78dc1ef9f20f2c15be693ac17dc0cd4f11e
곽진규 <146896149+btnl426@users.noreply.github.com>
2024-09-05 16:24:18 +0900

    [level 2] Title: 귤 고르기, Time: 45.35 ms, Memory: 26.4 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,19 +1,13 @@
+from collections import Counter
+
 def solution(k, tangerine):
-    # 1. 빈도 계산을 위한 딕셔너리 생성
-    count_dic = {}
+    # 1. 리스트의 요소 빈도 계산
+    counter = Counter(tangerine)
     
-    # 2. 리스트를 순회하면서 빈도 계산
-    for t in tangerine:
-        if t in count_dic:
-            count_dic[t] += 1
-        else:
-            count_dic[t] = 1
+    # 2. 빈도수를 기준으로 내림차순 정렬
+    sorted_elements = sorted(counter.items(), key=lambda x: x[1], reverse=True)
     
-    # 3. 빈도수를 기준으로 내림차순 정렬
-    # count_dic.items() -> (숫자, 빈도수) 튜플의 리스트
-    sorted_elements = sorted(count_dic.items(), key=lambda x: x[1], reverse=True)
-    
-    # 4. k 개 이상의 요소를 만들 수 있는 최소 요소 수 찾기
+    # 3. k 개 이상의 요소를 만들 수 있는 최소 요소를 찾기
     total_count = 0
     unique_count = 0
     
